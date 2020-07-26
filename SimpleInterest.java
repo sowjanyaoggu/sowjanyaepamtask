befd6ae6b55cce35f9f3a98cb88454e3c6e060f6
sowjanyaoggu <60355636+sowjanyaoggu@users.noreply.github.com>
2020-07-26 21:05:48 +0530

    Add files via upload

--- SimpleInterest.java
+++ SimpleInterest.java
@@ -1,11 +1,44 @@
-public class SimpleInterestCalculator extends InterestAmountDetails {
+import java.util.*;
+import  org.apache.log4j.*;
 
-    public SimpleInterestCalculator(double principleAmount, double rateOfInterest, double timePeriod){
-        super(principleAmount, rateOfInterest, timePeriod);
-    }
-    public double getTotalSimpleInterest(){
-        double interestAmount;
-        interestAmount = principleAmount * (1 + (rateOfInterest / 100.00) * timePeriod);
-        return interestAmount;
-    }
+class SimpleInterest{
+public double calculate(double principle,double time, double rate) {
+double simpleinterest = (principle * time * rate)/100;
+return simpleinterest;
+}
+}
+
+class CompoundInterest {
+public double calculate(double principle, double time, double rate) {
+    double amount = principle * (Math.pow((1 + rate / 100), time));
+    double compoundinterest = amount - principle;
+    return compoundinterest;
+   }
+}
+
+public class Interests {
+
+public static void main(String[] args) {
+// TODO Auto-generated method stub
+final Logger logger = Logger.getLogger(Interests.class);
+
+double simple,compound;
+double principle,time,rate;
+Scanner sc = new Scanner(System.in);
+System.out.println("enter principle time and rate");
+principle = sc.nextDouble();
+time = sc.nextDouble();
+rate = sc.nextDouble();
+
+SimpleInterest simpleinterest = new SimpleInterest();
+simple = simpleinterest.calculate(principle,time,rate);
+
+CompoundInterest compoundinterest = new CompoundInterest();
+compound = compoundinterest.calculate(principle,time,rate);
+
+logger.info("simple interest is"+simple);
+logger.info("compound interest is"+compound);
+sc.close();
+}
+
 }
